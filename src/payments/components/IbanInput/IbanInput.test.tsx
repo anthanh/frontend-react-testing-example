6a76966fe34a33735d8333c3356e1b94ed1a0ade
Anthanh Pham Trinh <antai.pt@gmail.com>
2019-02-25 12:51:20 +0100

    feat(payment): iban minor interaction with test and mount

--- src/payments/components/IbanInput/IbanInput.test.tsx
+++ src/payments/components/IbanInput/IbanInput.test.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import { shallow } from 'enzyme';
-import IbanInput from '.';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import IbanInput from ".";
 
+describe("IbanInput", () => {
+  let rendered;
+  it("renders as expected", () => {
+    rendered = shallow(<IbanInput />);
+    expect(rendered.html()).toMatchSnapshot();
+    expect(rendered.contains(<label htmlFor="iban">IBAN</label>)).toBe(true);
+    expect(rendered.find('TextField')).toHaveLength(1);
+  });
 
-describe('IbanInput', () => {
+  it("renders as expected", () => {
+    rendered = mount(<IbanInput />);
 
-  it('renders as expected', () => {
-    const rendered = shallow(<IbanInput></IbanInput>)
-    expect(rendered.html()).toMatchSnapshot()
-    expect(rendered.contains(<label htmlFor="iban">IBAN</label>)).toBe(true)
-    expect(rendered.find('TextField')).toHaveLength(1)
-  })
+    expect(rendered.find("input")).toHaveLength(1);
 
-})
+    // rendered
+    //   .find("IbanInput")
+    //   .instance()
+    //   .setValue({ target: { value: "customvalue" } });
+
+    // rendered.update();
+    // expect(rendered.find("input").props().value).toBe("customvalue");
+
+    rendered
+      .find("input")
+      .simulate("change", { target: { value: "customvalue" } });
+      expect(rendered.find("p").html()).toBe("<p class=\"iban-value\">customvalue</p>");
+    });
+});
--- src/payments/components/IbanInput/IbanInput.tsx
+++ src/payments/components/IbanInput/IbanInput.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component, SyntheticEvent, ChangeEvent } from "react";
 import TextField from "@material-ui/core/TextField";
 
-const IbanInput = () => (
-  <div>
-    <label htmlFor="iban">IBAN</label>
-    <TextField id="iban" placeholder="ES12 1234 1234 1234" />
-  </div>
-);
+interface IbanInputProps {
+  value?: string;
+  valid?: boolean;
+}
+
+class IbanInput extends Component<IbanInputProps, {}> {
+  state: IbanInputProps;
+
+  constructor(props: IbanInputProps) {
+    super(props);
+    this.state = {
+      value: "",
+      valid: true
+    };
+  }
+
+  setValue = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="iban">IBAN</label>
+        <TextField
+          id="iban"
+          placeholder="ES12 1234 1234 1234"
+          onChange={this.setValue}
+          value={this.state.value}
+        />
+        <p className="iban-value">{this.state.value}</p>
+      </div>
+    );
+  }
+}
 
 export default IbanInput;
